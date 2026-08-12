630dca09952fc85bf799df987aa049fc3528a848
Yefer <yefersonnjar02192510@gmail.com>
2026-08-12 17:27:05 -0500

    se agregaron imagenes y se creo html

--- xchango-usuario/src/app/app.config.ts
+++ xchango-usuario/src/app/app.config.ts
@@ -1,11 +1,8 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
 import { routes } from './app.routes';
 
-export const appConfig: ApplicationConfig = {
+export const appConfig = {
   providers: [
-    provideBrowserGlobalErrorListeners(),
     provideRouter(routes)
   ]
 };
---
+++ xchango-usuario/src/app/pages/login/login.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-login',
+  imports: [CommonModule],
+  templateUrl: './login.html',
+  styleUrl: './login.css'
+})
+export class LoginComponent {}
