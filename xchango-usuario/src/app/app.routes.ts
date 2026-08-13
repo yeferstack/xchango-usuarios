8f6486aa38f6fc57f344beb36cf21d24156b65c7
MIGUEL200401 <miguelperez.di2004@gmail.com>
2026-08-13 12:49:55 -0500

    eliminar

--- xchango-usuario/src/app/app.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Routes } from '@angular/router';
-import { PerfilComponent } from './pages/perfil/perfil';
-
-export const routes: Routes = [
-    {
-        path:'perfil',
-        component:PerfilComponent,
-    }
-];
