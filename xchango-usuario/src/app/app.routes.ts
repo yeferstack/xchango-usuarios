412790315d5214a6b014a7843e42a504e20729f4
Yefer <yefersonnjar02192510@gmail.com>
2026-08-13 12:45:21 -0500

    se elimino

--- xchango-usuario/src/app/app.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Routes } from '@angular/router';
-import { LoginComponent } from './pages/login/login';
-
-export const routes: Routes = [
-    {
-        path: 'login',
-        component: LoginComponent
-    }
-];
--- xchango-usuario/src/app/pages/login/login.ts
+++ xchango-usuario/src/app/pages/login/login.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+
 @Component({
   selector: 'app-login',
   imports: [CommonModule],
